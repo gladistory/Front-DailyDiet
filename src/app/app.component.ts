1f53104cd58bee53665c59ffe0f6987912716a74
Vinicius de Souza Santos <desouza850@gmail.com>
2025-06-25 20:53:19 -0300

    metade da tela de view metrics feita

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { UiBaseComponent } from './components/ui-base/ui-base.component';
 import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, UiBaseComponent, RouterOutlet],
+  imports: [NavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
